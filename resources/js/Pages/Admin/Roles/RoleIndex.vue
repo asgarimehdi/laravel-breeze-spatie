ae3e9386d72c1d540f7bc22ff1a9d6ed8bdb7044
mehdi asgari <it1@abharh.ir>
2023-10-21 13:33:49 +0330

    create role

--- resources/js/Pages/Admin/Roles/RoleIndex.vue
+++ resources/js/Pages/Admin/Roles/RoleIndex.vue
@@ -1,6 +1,6 @@
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
-import { Head } from '@inertiajs/vue3';
+import { Head,Link } from '@inertiajs/vue3';
 import TableHeaderCell from "@/Components/TableHeaderCell.vue";
 import TableDataCell from "@/Components/TableDataCell.vue";
 import Table from "@/Components/Table.vue";
@@ -13,7 +13,13 @@ defineProps(['roles'])
 
     <AdminLayout>
         <div class="max-w-7xl mx-auto py-4">
-            <h1>Roles Index Page</h1>
+            <div class="flex justify-between">
+                <h1>Roles Index Page</h1>
+                <Link :href="route('roles.create')" class="px-3 py-2 text-white bg-indigo-500 hover:bg-indigo-700 rounded">
+                    New Role
+                </Link>
+            </div>
+
         </div>
         <div class="mt-6">
             <Table>
